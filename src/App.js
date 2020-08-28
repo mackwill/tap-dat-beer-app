070bcdf1b569cb2ec667acd2ec63d77e1395a667
Will <mack.mindenhall@gmail.com>
2020-08-28 09:12:14 -0400

    Added basic Material UI app bar with a custom theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className="App">Tap-Dat Beer App</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>TAP DAT BEER APP</h1>
+    </div>
+  );
 }
 
 export default App;
