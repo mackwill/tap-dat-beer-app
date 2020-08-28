5b7ee7366f59b7f85c97e358b5a068539842360d
Will <mack.mindenhall@gmail.com>
2020-08-28 12:05:20 -0400

    Added state control to the login form. Also began adding the submit control but not full functionality yet.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
   const [registerOpen, setRegisterOpen] = useState(false);
   const [loginOpen, setLoginOpen] = useState(false);
+  const [state, setState] = useState({
+    username: null,
+    password: null,
+    currentUser: null,
+  });
 
   const handleLoginOpen = (e) => {
     console.log("Open Login modal");
-    // Uncomment this when the modal is here:
     setLoginOpen(true);
+  };
+
+  const handleLoginChange = (e) => {
+    e.persist();
+    setState((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value.trim(),
+    }));
+  };
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+
+    return axios
+      .post("/api/login", {
+        username: state.username,
+        password: state.password,
+      })
+      .then((data) => {
+        console.log("login promise data:  ", data);
+
+        // Need the server to return the user object after logging in
+        // and set the currentUser to the returned object
+        // setState(prev => ({
+        //   ...prev,
+        //   currentUser: data.data
+        // }))
+      })
+      .catch((err) => {
+        console.log("Login Error: ", err);
+      });
   };
 
   const handleRegisterOpen = (e) => {
@@ -18,11 +56,17 @@ function App() {
     // Uncomment this when the modal is here:
     setRegisterOpen(true);
   };
+
   return (
     <div className="App">
       <Navbar
         handleRegisterOpen={handleRegisterOpen}
         handleLoginOpen={handleLoginOpen}
+      />
+      <Login
+        open={loginOpen}
+        onChange={handleLoginChange}
+        handleClose={() => setLoginOpen(false)}
       />
       <h1>TAP DAT BEER APP</h1>
     </div>
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+// import CustomAlert from "../CustomAlert";
+
+export default function Login(props) {
+  // const handleOpen = () => {
+  //   setOpen(true);
+  // };
+
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
+
+  return (
+    <div>
+      <Dialog
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <form onSubmit={props.onSubmit}>
+          <DialogTitle id="form-dialog-title">
+            Login To Your Account!
+          </DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Username"
+              type="username"
+              name="username"
+              fullWidth
+              onChange={props.onChange}
+            />
+            <TextField
+              margin="dense"
+              id="name"
+              label="Password"
+              type="password"
+              name="password"
+              fullWidth
+              onChange={props.onChange}
+            />
+          </DialogContent>
+          {/* <CustomAlert errMessage={props.errMessage} /> */}
+          <DialogActions>
+            <Button onClick={props.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button type="submit" color="primary">
+              Login
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </div>
+  );
+}
