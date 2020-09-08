75af25016ae02420e3a29467dd637202dacf8535
Will <mack.mindenhall@gmail.com>
2020-09-08 19:11:59 -0400

    Updates

---
+++ precache-manifest.e299ca9f408c1ac034714980407f3caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368d81fbf3524ca8fdc770066a44616f",
+    "url": "/tap-dat-beer-app/index.html"
+  },
+  {
+    "revision": "95a09b0f037688b33b52",
+    "url": "/tap-dat-beer-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c6493d5495debc05a5f4",
+    "url": "/tap-dat-beer-app/static/css/main.6e697da3.chunk.css"
+  },
+  {
+    "revision": "95a09b0f037688b33b52",
+    "url": "/tap-dat-beer-app/static/js/2.a7b2c742.chunk.js"
+  },
+  {
+    "revision": "abc0e15ad50fca41a3559955f663274f",
+    "url": "/tap-dat-beer-app/static/js/2.a7b2c742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6493d5495debc05a5f4",
+    "url": "/tap-dat-beer-app/static/js/main.a12c1d50.chunk.js"
+  },
+  {
+    "revision": "f7698b40fad12dee1f82",
+    "url": "/tap-dat-beer-app/static/js/runtime-main.705a8ff1.js"
+  }
+]);
